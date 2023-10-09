54df98ce39a81d0b1042e1a6aceb48d2f1cecff4
BrianEtemesi <etemecci@gmail.com>
2023-10-09 11:47:21 +0300

    define PL team model

---
+++ PremierLeagueAPI/PremierLeagueAPI/Models/PLTeam.cs
@@ -0,0 +1,13 @@
+﻿namespace PremierLeagueAPI.Models
+{
+    public class PLTeam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MatchesPlayed { get; set; } = 0;
+        public int GoalsScored { get; set; } = 0;
+        public int GoalsConceded { get; set; } = 0;
+        public int Points { get; set; } = 0;
+
+    }
+}
